6cc4118cd7831a52350e59de9cb894ad61dfbb3a
Neil G <ngaskell@gmail.com>
2023-07-15 12:23:59 +0100

    Some examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function MyButton({count,onClick}){
+  
+
+
+
+  return (
+    <button onClick={onClick}
+    className='button'
+    >I'm a button clicked {count} times</button>
+  );
+}
+
+const user = {
+  name: 'neil',
+  url: 'https://www.google.com'
+};
 
 function App() {
+  const [count,setCount] = useState(0);
+  
+  function handleClick(){
+    setCount(count + 1);
+  }
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {user.name}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={user.url}
           target="_blank"
           rel="noopener noreferrer"
         >
           My First React App
+          
         </a>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
       </header>
     </div>
   );
